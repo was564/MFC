6e699c1cba0ab989a4a412b056c43215d40aec96
was564 <40713340+was564@users.noreply.github.com>
2019-05-07 14:49:48 +0900

    Add files via upload
    윈도우 프로그래밍
    사각형 그림

---
+++ MFCApplication1/MFCApplication1/MFCApplication1.h
@@ -0,0 +1,35 @@
+﻿
+// MFCApplication1.h: MFCApplication1 응용 프로그램의 기본 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'stdafx.h'를 포함합니다."
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// CMFCApplication1App:
+// 이 클래스의 구현에 대해서는 MFCApplication1.cpp을(를) 참조하세요.
+//
+
+class CMFCApplication1App : public CWinApp
+{
+public:
+	CMFCApplication1App() noexcept;
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 구현입니다.
+
+public:
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCApplication1App theApp;
---
+++ MFCApplication1/MFCApplication1/resource.h
@@ -0,0 +1,27 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCApplication1.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_MFCApplication1TYPE         130
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_RED                          32774
+#define ID_GREEN                        32775
+#define ID_BLUE                         32776
+#define ID_COLOR_BLUE                   32777
+#define ID_COLOR_RED                    32778
+#define ID_COLOR_GREEN                  32779
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32780
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
